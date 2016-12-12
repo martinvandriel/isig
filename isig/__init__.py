1199f986a8bce0cb46a34323ba8716fdefd20601
martinvandriel <vandriel@erdw.ethz.ch>
2016-12-12 13:57:01 +0100

    adding __init__.py

---
+++ isig/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__init__
+
+:copyright:
+    Martin van Driel (Martin@vanDriel.de), 2016
+:license:
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .version import get_git_version
+
+__version__ = get_git_version()
